a3c7d31b30333d6d27d65875ed15c4340cc5f657
lnnd <leonid.postbox@gmail.com>
2024-05-05 08:24:21 +0300

    task1

---
+++ HW6/HW6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void task1();
+
+int main()
+{
+    task1();
+}
+
+void task1()
+{
+    int num = 0;
+    int step = 0;
+    int lastNum = 0;
+
+    std::cout << "Enter 3 numbers separated by a space (first number, step, last number)" << std::endl;
+    std::cin >> num >> step >> lastNum;
+
+    while (num <= lastNum)
+    {
+        std::cout << num << std::endl;
+        num += step;
+    }
+}
