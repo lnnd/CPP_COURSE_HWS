76bd0a9d39ee3fca5b1cba4fcc80b7488b3e114b
lnnd <leonid.postbox@gmail.com>
2024-05-05 08:46:56 +0300

    task2

--- HW6/HW6.cpp
+++ HW6/HW6.cpp
@@ -1,10 +1,13 @@
 ﻿#include <iostream>
 
 void task1();
+void task2();
 
 int main()
 {
     task1();
+    
+    task2();
 }
 
 void task1()
@@ -21,4 +24,30 @@ void task1()
         std::cout << num << std::endl;
         num += step;
     }
+}
+
+void task2()
+{
+    int num = 0;
+    
+    std::cout << "Enter the Fibonacci sequence number" << std::endl;
+    std::cin >> num;
+
+    if (num <= 0)
+    {
+        std::cout << "Error number";
+        return;
+    }
+
+    int f1 = 1;
+    int f2 = 0;
+    
+    for (f1; num > 0; num--)
+    {
+        int a = f1;
+        f1 += f2;
+        f2 = a;
+    }
+
+    std::cout << f1 << std::endl;
 }
