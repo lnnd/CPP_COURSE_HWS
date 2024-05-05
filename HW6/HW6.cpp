ddf2faea0335db573dd351891b1cc7be8b218d2d
lnnd <leonid.postbox@gmail.com>
2024-05-05 08:55:12 +0300

    task3

--- HW6/HW6.cpp
+++ HW6/HW6.cpp
@@ -2,12 +2,15 @@
 
 void task1();
 void task2();
+void task3();
 
 int main()
 {
     task1();
     
     task2();
+    
+    task3();
 }
 
 void task1()
@@ -50,4 +53,27 @@ void task2()
     }
 
     std::cout << f1 << std::endl;
+}
+
+void task3()
+{
+    int num = 0;
+
+    std::cout << "Enter a positive integer" << std::endl;
+    std::cin >> num;
+
+    if (num <= 0)
+    {
+        std::cout << "Error number";
+        return;
+    }
+
+    int result = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+
+    std::cout << "Result: " << result << std::endl;
 }
