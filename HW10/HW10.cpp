aa6146b77e92126ade9dfda549ebd936e66eda1d
John Doe <leonid.postbox@gmail.com>
2024-05-21 18:11:59 +0300

    task1

--- HW10/HW10.cpp
+++ HW10/HW10.cpp
@@ -1,9 +1,32 @@
 ﻿
 #include <iostream>
 
+void task1();
+
 int main()
 {
-    
+    task1();
 }
 
+void swap(int& a, int& b) 
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void task1()
+{
+    int a = 10;
+    int b = 20;
+
+    swap(a, b);
+    swap(&a, &b);
+}
 
