568e2958a26f84b86e73b970c2b9f93103aad5a5
John Doe <leonid.postbox@gmail.com>
2024-06-23 12:35:52 +0300

    HW18

--- HW18/Coffee.cpp
+++ HW18/Coffee.cpp
@@ -3,25 +3,25 @@
 
 void Espresso::prepare()
 {
-	std::cout << "Preparing: " << "Espresso" << std::endl;
+	std::cout << "Preparing: " << "Espresso" << "(" << getSize() << ")" << std::endl;
 	std::cout << "Ingredients: 1 spoons of coffee, 1 spoon of sugar" << std::endl;
 }
 
 void Cappuccino::prepare()
 {
-	std::cout << "Preparing: " << "Cappuccino" << std::endl;
+	std::cout << "Preparing: " << "Cappuccino" << "(" << getSize() << ")" << std::endl;
 	std::cout << "Ingredients: 2 spoons of coffee, 1 spoon of sugar, milk" << std::endl;
 }
 
 void Latte::prepare()
 {
-	std::cout << "Preparing: " << "Latte" << std::endl;
+	std::cout << "Preparing: " << "Latte" << "(" << getSize() << ")" << std::endl;
 	std::cout << "Ingredients: 2 spoons of coffee, 2 spoon of sugar, milk" << std::endl;
 }
 
 void Americano::prepare()
 {
-	std::cout << "Preparing: " << "Americano" << std::endl;
+	std::cout << "Preparing: " << "Americano" << "(" << getSize() << ")" << std::endl;
 	std::cout << "Ingredients: 2 spoons of coffee, 1 spoon of sugar" << std::endl;
 }
 
