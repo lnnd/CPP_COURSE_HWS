6a391ed6b7578e78320c9967e97cfe795416a336
John Doe <leonid.postbox@gmail.com>
2024-07-14 20:14:48 +0300

    Task2()

--- HW21/HW21.cpp
+++ HW21/HW21.cpp
@@ -3,16 +3,17 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <functional>
 
 void Task1();
+void Task2();
 
 int main()
 {
     Task1();
-
-
-
-
+    
+	Task2();
+	
 	return 0;
 }
 
@@ -81,4 +82,29 @@ void Task1()
 
 	stringContainer = getStringContainer(); // Reset the container
 	std::sort(stringContainer.begin(), stringContainer.end(), lambdaByLenth);	
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/////////////// Task 2 
+
+unsigned int countDivisibleBy(const std::vector<int>& vec, int number) 
+{
+	auto lambda = [number](int i) 
+	{ 
+		return i % number == 0;
+	};
+
+	unsigned int result = std::count_if(vec.begin(), vec.end(), lambda);
+
+	return result;
+}
+
+void Task2()
+{
+	std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+	int number = 3;
+
+	unsigned int count = countDivisibleBy(vec, number);
+
+	std::cout << "The number of elements divisible by " << number << " is " << count << std::endl;
 }
