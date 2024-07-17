8c2e3dc4414812eb2b73b9fb5fd179ecc44bbf4e
John Doe <leonid.postbox@gmail.com>
2024-07-17 18:28:13 +0300

    HW22 start

---
+++ DynamicArrayHW/DynamicArray.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <cstdlib>
+
+//#TODO: Make it template class
+//int type should be replace with smth more general
+class DynamicArray
+{
+public:
+    DynamicArray();
+    DynamicArray(std::size_t size);
+    DynamicArray(const DynamicArray& other);   //c-py c-tor, #TODO,change signature if needed
+
+    ~DynamicArray();
+
+    DynamicArray& operator=(const DynamicArray& other); //assignment operator, #TODO, change signature if needed
+
+    //validate index always or only in _DEBUG configuration
+    int& operator[](std::size_t index);
+
+    void setSize(std::size_t newSize);
+    std::size_t getSize() const;
+
+    void clear();
+
+    // Allocate new memory for new element
+    // Copy old content to new inner array
+    // insert element at the last index
+    // update size
+    void push_back(int element);
+
+
+    //#TODO: HW new methods
+    void pop_back();    //remove last element
+    int back() const;   //get last element
+
+    //https://cplusplus.com/reference/vector/vector/reserve/
+    //used to allocate more memory then needed right now
+    //to greatly decrease number of heap call for memory reallocation
+    //and number of operations to copy old data to new memory chunk
+    void reserve(std::size_t reservedSpace);
+    std::size_t getCapacity() const;    //returns reserve size. Should be ALWAYS >= then actual size
+
+    //reduce the capacity to fit its size
+    //https://cplusplus.com/reference/vector/vector/shrink_to_fit/
+    void shrinkToFit();
+
+    bool operator==(const DynamicArray& other) const;    //#TODO, change signature if needed
+private:
+
+};
