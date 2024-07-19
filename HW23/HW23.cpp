93533ebfe5d4593f89c0112b42cf35b3d55c87a0
John Doe <leonid.postbox@gmail.com>
2024-07-19 18:56:31 +0300

    HW23 task2

--- HW23/HW23.cpp
+++ HW23/HW23.cpp
@@ -1,13 +1,21 @@
 ﻿
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <sstream>
 
 void Task1();
+void Task2();
 
 int main()
 {
-    Task1();
-
+    //Task1();
+	Task2();
+	
+	
+	
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////
@@ -39,4 +47,34 @@ void Task1()
 {
 	unsigned int result = uniqueWordsCount("Hello Hey Hello Bye Hey");
 	std::cout << "Number of different words - " << result << std::endl;
+}
+
+////////////////////////////////////////////////////////////////////
+///////////////// task 2
+
+std::string mostOccuredWord(const std::string& line)
+{
+	std::unordered_map<std::string, int> wordCount; 
+	std::stringstream ss(line); 
+	std::string word;
+	std::string mostOccuredWord;
+	int maxCount = 0;
+
+	while (ss >> word) {
+		++wordCount[word]; 
+	}
+
+	for (const auto& wc : wordCount) {
+		if (wc.second > maxCount) {
+			maxCount = wc.second;
+			mostOccuredWord = wc.first;
+		}
+	}
+	return mostOccuredWord;
+}
+
+void Task2()
+{
+	std::string result = mostOccuredWord("Hello Hey Hello Bye Hey Hey");
+	std::cout << "Most occured word - " << result << std::endl;
 }
