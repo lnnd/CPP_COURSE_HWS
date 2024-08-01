b82d098da27f68aed8903f52294dbd88d86a45d1
John Doe <leonid.postbox@gmail.com>
2024-08-01 20:00:56 +0300

    HW27 start + task1

---
+++ HW27/HW27.cpp
@@ -0,0 +1,22 @@
+﻿
+#include "Vector2d.h"
+
+void task1();
+
+
+int main()
+{
+	task1();
+
+
+
+
+	return 0;
+}
+
+void task1()
+{
+	Vector2d vector = Vector2d(1.0f, 1.0f, 3.0f, 3.0f);
+	vector.printVector();
+}
+
